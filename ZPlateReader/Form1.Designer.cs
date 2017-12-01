b29be7be30055294984e4da5a7ad96c705c9f661
Herald Gersdorff <left1dead@yandex.ru>
2017-12-01 21:45:59 +0300

    0

--- ZPlateReader/Form1.Designer.cs
+++ ZPlateReader/Form1.Designer.cs
@@ -28,12 +28,44 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.openFileDialog_main = new System.Windows.Forms.OpenFileDialog();
+			this.button_test = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// openFileDialog_main
+			// 
+			this.openFileDialog_main.Filter = "Jpeg files (*.jpg)|*.jpg|Bmp files (*.bmp)|*.bmp|Png files (*.png)|*.png|All file" +
+    "s (*.*)|*.*";
+			this.openFileDialog_main.FilterIndex = 4;
+			this.openFileDialog_main.RestoreDirectory = true;
+			// 
+			// button_test
+			// 
+			this.button_test.Font = new System.Drawing.Font("Microsoft Sans Serif", 23.77358F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button_test.Location = new System.Drawing.Point(12, 12);
+			this.button_test.Name = "button_test";
+			this.button_test.Size = new System.Drawing.Size(208, 73);
+			this.button_test.TabIndex = 0;
+			this.button_test.Text = "hit meh";
+			this.button_test.UseVisualStyleBackColor = true;
+			this.button_test.Click += new System.EventHandler(this.button_test_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Text = "Form1";
+			this.ClientSize = new System.Drawing.Size(784, 559);
+			this.Controls.Add(this.button_test);
+			this.Name = "Form1";
+			this.Text = "ZPlateReader";
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.OpenFileDialog openFileDialog_main;
+		private System.Windows.Forms.Button button_test;
 	}
 }
 
--- ZPlateReader/Form1.cs
+++ ZPlateReader/Form1.cs
@@ -1,20 +1,90 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
+using System.IO;
+using System.Runtime.InteropServices;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace ZPlateReader
 {
 	public partial class Form1 : Form
 	{
+		private struct ANPR_OPTIONS
+		{
+			public int min_plate_size; // Минимальная площадь номера
+			public int max_plate_size; // Максимальная площадь номера
+			public int Detect_Mode; // Режимы детектирования	
+			public int max_text_size; // Максимальное количество символов номера + 1
+			public int type_number; // Тип автомобильного номера	
+			public int flags; // Дополнительные опции
+		};
+
+		[ DllImport( "iANPRinterface_vc12_x64.dll", CallingConvention = CallingConvention.StdCall ) ]
+		private static extern int anprPlateMemoryXML( byte[] in_buffer,
+			int size_buffer,
+			ANPR_OPTIONS Options,
+			StringBuilder xml_buffer,
+			int[] size_xml_buffer );
+
 		public Form1()
 		{
 			InitializeComponent();
+		}
+
+		private void button_test_Click( object sender, EventArgs e )
+		{
+			if ( openFileDialog_main.ShowDialog() == DialogResult.OK )
+			{
+				try
+				{
+					using ( var stream = openFileDialog_main.OpenFile() )
+					{
+						var size = ( int ) stream.Length;
+						var buffer = new byte[ size ];
+
+						using ( var memory_stream = new MemoryStream() )
+						{
+							int temp;
+
+							while ( ( temp = stream.Read( buffer, 0, buffer.Length ) ) > 0 )
+								memory_stream.Write( buffer, 0, temp );
+						}
+
+						var anpr_options = new ANPR_OPTIONS
+						{
+							Detect_Mode = 14,
+							min_plate_size = 500,
+							max_plate_size = 25000,
+							max_text_size = 20,
+							type_number = 0,
+							flags = 7
+						};
+
+						var buffer_builder = new StringBuilder( 10000 );
+						int[] size_builder = { 10000 };
+
+						var very_temp = anprPlateMemoryXML( buffer, size, anpr_options, buffer_builder, size_builder );
+
+						using ( var reader = XmlReader.Create( new StringReader( buffer_builder.ToString() ) ) )
+						{
+							reader.ReadToFollowing( "allnumbers" );
+							reader.MoveToFirstAttribute();
+
+							for ( int i = 0, end = int.Parse( reader.Value ); i < end; ++i )
+							{
+								reader.ReadToFollowing( "number" );
+								reader.MoveToFirstAttribute();
+
+								Console.WriteLine( reader.Value );
+							}
+						}
+					}
+				}
+				catch ( Exception exception )
+				{
+					MessageBox.Show( "Something reaaaally bad happened.." + Environment.NewLine + exception.Message, @".//Error", MessageBoxButtons.OK, MessageBoxIcon.Error );
+				}
+			}
 		}
 	}
 }
